62f30f50b0c1e85ece822ef91646af89ebc29145
Fernandobika <alves.rodrigues.fernando@escola.pr.gov.br>
2025-09-02 11:17:28 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,6 +1,45 @@
 const perguntas = [
 {
     enunciado: "perdunta 1" , 
-    alternativas
-}
-]
+    alternativas:[
+{
+    text: "nike",
+    afirmação:[
+        "voce tem bom gosto",
+        "afirmação 2"
+    ],
+},
+{
+   texto: "adidas",
+   afirmação: [
+    "voce e valido",
+    "voce não sabe escolher",
+         ],
+       }
+    ]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativas:[
+            {
+        texto: "nike",
+        afirmação: [
+            "voce tem bom gosto",
+            "afirmação 2",
+            ],
+        },
+     {
+        texto:"adidas",
+        afirmação:[
+            "voce e vaiado",
+            "voce não sabe escolher"
+            ]
+        }
+    ]
+},
+{
+    enunciado: "pergunta 4"
+    alternativas:{
+        "voce ter bom gosto",
+        "afirmação 2"
+    }
