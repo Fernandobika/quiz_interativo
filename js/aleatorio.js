a65cffb67ab7c36949cc4daaa37388f33bcb0b57
Fernandobika <alves.rodrigues.fernando@escola.pr.gov.br>
2025-09-04 12:04:45 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,1 +1,8 @@
+cont nomes = ["João", "Emerson", "Luke", "Pipokinha", "Cleiton", "Ana"];
 
+funcion aleatorio(lista){
+  const posição = Math.floor(Math.random()
+  return lista[posicao];
+}
+
+const nome = aleatorio(nomes)
